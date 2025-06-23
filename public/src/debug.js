2eadb20ea17f44bb9c3bed16137899114f801be5
nsendek <nsendek1999@gmail.com>
2025-06-23 13:21:46 -0700

    cleaning up debug p5 sketch

--- public/src/debug.js
+++ public/src/debug.js
@@ -5,10 +5,13 @@ export const sketch = (p) => {
   let video;
   let poses = [];
   let averagePoses = [];
-  let videoToCanvasScale = 0.75
+
+  let aspectRatio = 16 / 9;
   let relevantIndices = [];
+  let videoHeight = 240;
+
   p.setup = () => {
-    const canvas = p.createCanvas(640 * videoToCanvasScale, 480 * videoToCanvasScale);
+    const canvas = p.createCanvas(videoHeight * aspectRatio, videoHeight);
     canvas.elt.addEventListener('click', () => {
       console.log(poses);
     });
@@ -60,31 +63,31 @@ export const sketch = (p) => {
   }
 
   p.draw = () => {
-    p.clear();
-
+    p.clear();    
     p.image(video, 0, 0, p.width, p.height);
+    p.filter(p.GRAY);
 
     averagePoses.forEach((pose, k) => {
       if (!pose) return;
+      p.push();
       if (config.drawCenterPointInDebug) {
-        p.fill('white');
-        p.circle(pose.center.x * p.width, pose.center.y * p.height, 15);
+        setFillOrStroke(k);
+        triangle(pose.center.x * p.width, pose.center.y * p.height, 10);
       }
       relevantIndices.forEach(i => {
         let point = pose.landmarks[i];
         if (!point) return;
-        p.push();
         setFillOrStroke(k);
-        p.noStroke();
         p.circle(point.x * p.width, point.y * p.height, 5);
-        p.pop();
       })
+      p.pop();
     });
 
     poses.forEach((pose, k) => {
       if (!pose || !pose.landmarks) {
         return;
       }
+      p.push();
       if (pose.bbox && config.drawPoseBoundingBox) {
         p.stroke('white');
         p.noFill();
@@ -93,15 +96,30 @@ export const sketch = (p) => {
         const yDiff = max.y - min.y;
         p.rect(min.x * p.width, min.y * p.height, xDiff * p.width, yDiff * p.height)
       }
+      setFillOrStroke(k, true);
       relevantIndices.forEach(i => {
         let point = pose.landmarks[i];
         if (!point) return;
-        p.push();
-        setFillOrStroke(k, true);
         p.circle(point.x * p.width, point.y * p.height, 10);
-        p.pop();
       })
+      p.pop();
     });
+  }
+
+  function triangle(x, y, side) {
+    const theta = Math.PI / 3;
+    const x1 = side * p.cos(0);
+    const y1 = side * p.sin(0);
+    const x2 = side * p.cos(2 * theta);
+    const y2 = side * p.sin(2 * theta);
+    const x3 = side * p.cos(4 * theta);
+    const y3 = side * p.sin(4 * theta);
+
+    p.push()
+    p.translate(x, y);
+    p.rotate(-Math.PI / 2);
+    p.triangle(x1, y1, x2, y2, x3, y3);
+    p.pop();
   }
 
   function setFillOrStroke(poseId, isStroke) {
